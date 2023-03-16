46764cfb8b9cef6ae30b959ff3c741a2e25a320c
Alden Kim <aldenk51@gmail.com>
2023-03-16 17:16:39 -0400

    Added basic functions for operators

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+/**
+ * Takes two inputs and returns the sum
+ * @param {Number} input1
+ * @param {Number} input2 
+ * @returns input1 + input2
+ */
+function add(input1, input2) {
+  return input1+input2;
+}
+
+
+/**
+ * Takes two inputs and returns the difference
+ * @param {Number} input1 
+ * @param {Number} input2 
+ * @returns input1 - input2
+ */
+function subtract(input1, input2) {
+  return input1-input2;
+}
+
+
+/**
+ * Takes two inputs and returns the product
+ * @param {Number} input1 
+ * @param {Number} input2 
+ * @returns input1 * input2
+ */
+function multiply(input1, input2) {
+  return input1*input2;
+}
+
+
+/**
+ * Takes two inputs and returns the quotient
+ * @param {Number} input1 
+ * @param {Number} input2 
+ * @returns input1 / input2
+ */
+function divide(input1, input2) {
+  return input1/input2;
+}
